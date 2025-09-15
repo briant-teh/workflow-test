c5d7d77f46ff584f50b146d63b1fdec80a856523
Briant Teh <briant_teh@hotmail.co.uk>
2025-09-15 14:38:35 +0800

    add workflow job

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
     println!("Hello, world!");
+
+    let x = 12;
+    let y = 39;
+
+    println!("The sum pf {x} and {y} is {}", sum(x, y))
+}
+
+fn sum(x: i64, y: i64) -> i64 {
+    x + y
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_sum() {
+        assert_eq!(sum(3, 5), 8)
+    }
 }
